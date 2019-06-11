5e9d1943b1c824209e940ca2345552ad62b1609b
jlecraft <jlecraft@gmail.com>
2019-06-11 09:53:46 -0700

    adding file

---
+++ num_generator.rb
@@ -0,0 +1,14 @@
+start = 1
+counter = 0
+maxCounter = 2
+
+53.times do |n|
+	if (counter > maxCounter) then
+		counter = 0
+		start = start + 1
+		maxCounter = maxCounter + 1
+	end
+
+	counter = counter + 1
+	p start
+end
