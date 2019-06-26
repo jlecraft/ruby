f5c5108624c8300f06800f619499769f192a8095
jlecraft <jlecraft@gmail.com>
2019-06-25 17:05:11 -0700

    Added more dynamic way to create list

--- combinatorics.rb
+++ combinatorics.rb
@@ -1,11 +1,12 @@
-statList = %i(haste crit crit_dmg dodge a b c d e f g h i j k l m n o p)
+# statList = %i(1 2 3 4 5 6 7 8 )
+statList = [*(1..20)].map { |n| n < 10 ? "0" + n.to_s : n.to_s }
 
 
 # myList.permutation(2).each do |pair|
 statList.each { |stat| puts stat }
 
 total = statList.size
-statList.repeated_combination(2).each do |pair|
+statList.repeated_combination(3).each do |pair|
 	unless (pair.first == pair.last) then
 		puts pair.join(", ")
 		total += 1
