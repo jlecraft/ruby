8e75d9df184234e024c8a8f6a4bc8ea89d12486a
jlecraft <jlecraft@gmail.com>
2019-07-08 08:04:01 -0700

    Weekend update

--- counter.rb
+++ counter.rb
@@ -1,25 +1,21 @@
 class Counter
-	def initialize(arg, options = {})
-		if (arg =~ /(?:(\d+) d)? (\d+) \s* (?: \+ \s* (\d+))?/x)
-			@count = ($1 || 1).to_i
-			@max = $2.to_i
-			@bonus = ($3 || 0).to_i
-		end
-
-		@digits = Array.new(@count, 1) 
+	# Expect a variable number of arrays
+	def initialize(*args)
+		@dice = args.dup
+		@indices = Array.new(@dice.size, 0)
 	end
 	
 	def reset
-		@digits.map! { 1 }
+		@indices.map! { 0 }
 		self
 	end
 
 	def next!
-		@digits.each_with_index do |v,i|
-			if (v == @max)
-				@digits[i] = 1
+		@indices.each_with_index do |v, i|
+			if (v >= (@dice[i].size - 1))
+				@indices[i] = 0
 			else
-				@digits[i] += 1
+				@indices[i] += 1
 				break
 			end
 		end
@@ -27,24 +23,53 @@ class Counter
 	end
 	
 	def zero?
-		@digits.all? { |v| v == 1 }
+		@indices.all? { |v| v == 0 }
 	end
 	
-	def max?
-		@digits.all? { |v| v == @max }
-	end
-	
+	# def each
+	# 	yield(@indices)
+	# 	yield(@indices) until (next!.zero?)
+	# end
+
 	def each
-		yield(@digits)
-		yield(@digits) until (next!.zero?)
+		yield(@indices.each_with_index.map { |idx, i| @dice[i][idx] })
+		next!
+
+		while (not zero?) do
+			yield(@indices.each_with_index.map { |idx, i| @dice[i][idx] })
+			next!
+		end
 	end
 	
 	def inspect
-		@digits
+		@indices
 	end
 end
 
-c = Counter.new("2d20") #, drop_lowest: 1)
+d4 = [*(1..4)]
+d6 = [*(1..6)]
+d8 = [*(1..8)]
+d10 = [*(1..10)]
+d12 = [*(1..12)]
 
-c.each { |a| p a }
+special1 = [1, 2, 3, 4, 5, 5] # 21
+special2 = [1, 2, 2, 2, 3, 7]
 
+
+c = Counter.new(d6, d6, d6, d6, d6, d6)
+
+totals = Hash.new(0)
+
+outputFile = File.new("counter.out", "w+")
+
+c.each do |dieSet|
+	dieSum = dieSet.inject(:+)
+	totals[dieSum] += 1
+	outputFile.printf("%s\n", dieSet.sort.join("\t"))
+end
+
+tmp = totals.map do |k, v|
+	"#{k} = #{v}"
+end
+
+puts tmp
