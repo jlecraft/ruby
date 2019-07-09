6010cc7b135d1537fae3cd0657c54d1f6105a0cb
jlecraft <jlecraft@gmail.com>
2019-07-09 09:13:57 -0700

    Array with memory of current index

---
+++ memory_array.rb
@@ -0,0 +1,34 @@
+class MemoryArray < Array
+	def initialize(*args)
+		super(*args)
+		@currentIndex = 0
+	end
+
+	def value
+		self[@currentIndex]
+	end
+
+	def next
+		lastValue = value
+		
+		if ((@currentIndex + 1) < size) then
+			@currentIndex += 1
+		else
+			@currentIndex = 0
+		end
+
+		lastValue
+	end
+
+	def inspect
+		self.each_with_index.map { |e, i| (i == @currentIndex ? " (#{e})" : sprintf("%4d", e))}.join(" ")
+	end
+end
+
+m = MemoryArray.new([*(1..6)])
+
+
+8.times do |n|
+	p m.next
+end
+
