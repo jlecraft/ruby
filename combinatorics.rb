409583586f7c35131560e55a9a90a578e05f65ac
jlecraft <jlecraft@gmail.com>
2019-07-09 18:15:57 -0700

    demonstration of different built-in set functions

--- combinatorics.rb
+++ combinatorics.rb
@@ -1,16 +1,47 @@
 # statList = %i(1 2 3 4 5 6 7 8 )
+coinFlip = [:heads, :tails]
+smallList = [:A, :B, :C]
 statList = [*(1..20)].map { |n| n < 10 ? "0" + n.to_s : n.to_s }
 
-
-# myList.permutation(2).each do |pair|
-statList.each { |stat| puts stat }
-
-total = statList.size
-statList.repeated_combination(3).each do |pair|
-	unless (pair.first == pair.last) then
-		puts pair.join(", ")
-		total += 1
-	end
+puts "Permutation: #{smallList}"
+smallList.permutation.each do |x|
+	puts x.join(', ')
 end
 
-puts "#{total} combinations"
+puts "Combination(2): #{smallList}"
+smallList.combination(2).each do |x|
+	puts x.join(', ')
+end
+
+puts "Repeated Combinations(2): #{smallList}"
+smallList.repeated_combination(2).each do |x|
+	puts x.join(', ')
+end
+
+puts "Repeated Permutation(2): #{smallList}"
+smallList.repeated_permutation(2).each do |x|
+	puts x.join(', ')
+end
+
+# coinFlip.permutation.each do |ele|
+# 	puts ele.join(', ')
+# end
+
+# statList.combination(2).each do |ele|
+# 	puts ele.join(', ')
+# end
+
+
+
+# # myList.permutation(2).each do |pair|
+# statList.each { |stat| puts stat }
+
+# total = statList.size
+# statList.repeated_combination(3).each do |pair|
+# 	unless (pair.first == pair.last) then
+# 		puts pair.join(", ")
+# 		total += 1
+# 	end
+# end
+
+# puts "#{total} combinations"
