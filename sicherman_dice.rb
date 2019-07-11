67fc88a5b89b851dd3107d63c865317b6e166260
jlecraft <jlecraft@gmail.com>
2019-07-10 20:01:20 -0700

    Clean up

--- sicherman_dice.rb
+++ sicherman_dice.rb
@@ -1,30 +1,9 @@
-class Die
-	
+require_relative 'counter'
+
+c = Counter.new([1, 2, 2, 3, 3, 4], [1, 3, 4, 5, 6, 8])
+
+puts '-' * 100
+
+c.each do
+	printf("%3d: %s\n", c.sum, c.inspect)
 end
-
-die1 = [1, 3, 4, 5, 6, 8]
-die2 = [1, 2, 2, 3, 3, 4]
-
-totals = Hash.new(0)
-pairs = Array.new
-
-die_set = Array.new << die1 << die2
-
-die_set.each do |die|
-
-end
-
-
-# die1.each do |a|
-# 	die2.each do |b|
-# 		totals[(a + b)] += 1
-# 		pairs << [a, b]
-# 		# puts "#{a} #{b} (#{a + b})" + (a == b ? " (D)" : "")
-# 	end
-# end
-
-# pairs.sort { |a, b| (a.first + a.last) <=> (b.first + b.last) }.each do |x|
-# 	printf("%2d: %d, %d\n", x.inject(:+), x[0], x[1])
-# end
-
-# puts totals.map { |k, v| "#{k} (#{v})" }.join(", ")
